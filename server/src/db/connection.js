db4ade5f78147af83d76a0c077403ff21a5ad929
Abhaya N K <abhayakhatri48@gmail.com>
2024-07-18 19:01:54 +0545

    mongodb connection setup

---
+++ server/src/db/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const connect = async() => {
+    const connected = await mongoose.connect('mongodb://127.0.0.1:27017/cineworldDB');
+    if(connected){
+        console.log("connected to mongodb")
+    }else{
+        console.log("connection failed")
+    }
+}
+module.exports = connect
